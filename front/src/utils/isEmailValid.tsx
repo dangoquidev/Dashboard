415edc53d01e137b7f766efcf43555638d5d9f6c
Alexandre De Almeida <91562207+Pikaleks@users.noreply.github.com>
2024-01-28 23:26:46 +0100

    feat:  better email validator

--- front/src/utils/isEmailValid.tsx
+++ front/src/utils/isEmailValid.tsx
@@ -1,6 +1,9 @@
-const validateEmail = (email: string) => email.match(/^[A-Z0-9._%+-]+@[A-Z0-9.-]+.[A-Z]{2,4}$/i);
+import { verifyEmail } from '@devmehq/email-validator-js';
 
-export const isEmailValid = (email: string) => {
+export const isEmailValid = async (email: string) => {
     if (email === "") return false;
-    return validateEmail(email) ? true : false;
+    const { validFormat} = await verifyEmail({ emailAddress: email, timeout: 3000 });
+    console.log(validFormat);
+    if (!validFormat) return false;
+    return true;
 }
