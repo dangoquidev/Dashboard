861e32eaaccf97feec60a726d344908225e27c76
Alexandre De Almeida <91562207+Pikaleks@users.noreply.github.com>
2024-01-28 23:27:35 +0100

    feat: dashboard route

--- front/src/router.tsx
+++ front/src/router.tsx
@@ -1,8 +1,10 @@
 import { Navigate, Route, Routes } from "react-router-dom";
-import React, { ReactElement } from "react";
+import { ReactElement } from "react";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
 import OutletWrapper from "./utils/OutletWrapper";
+import DashboardLayout from "./Components/Dashboard/DashboardLayout";
+import DashboardApp from "./pages/DashboardApp";
 
 export const Router = (): ReactElement => {
 	return (
@@ -11,6 +13,10 @@ export const Router = (): ReactElement => {
 				<Route path='login' element={<Login />} />
 				<Route path='register' element={<Register />} />
 				<Route path='' element={<Navigate to='/login' />} />
+			</Route>
+			<Route path="/dashboard" element={<DashboardLayout />}>
+				<Route path="" element={<Navigate to="/dashboard/app" replace />} />
+				<Route path="app" element={<DashboardApp />} />
 			</Route>
 		</Routes>
 	);
