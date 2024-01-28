f4252466e8e7666d158ff4b43d2c651dbfeeba79
Alexandre De Almeida <91562207+Pikaleks@users.noreply.github.com>
2024-01-28 23:27:56 +0100

    feat: dashboard main page

---
+++ front/src/pages/DashboardApp.tsx
@@ -0,0 +1,11 @@
+import { Button } from "@nextui-org/react"
+
+const DashboardApp = () => {
+    return (
+        <div>
+            <Button color="primary">Button</Button>
+        </div>
+    )
+}
+
+export default DashboardApp
