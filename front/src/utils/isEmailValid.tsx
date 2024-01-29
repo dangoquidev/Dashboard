63ac398a6724eb207944e1f1e649d0c68bf4aeb0
Alexandre De Almeida <91562207+Pikaleks@users.noreply.github.com>
2024-01-29 23:50:38 +0100

    DEL: console.log

--- front/src/utils/isEmailValid.tsx
+++ front/src/utils/isEmailValid.tsx
@@ -3,7 +3,6 @@ import { verifyEmail } from '@devmehq/email-validator-js';
 export const isEmailValid = async (email: string) => {
     if (email === "") return false;
     const { validFormat} = await verifyEmail({ emailAddress: email, timeout: 3000 });
-    console.log(validFormat);
     if (!validFormat) return false;
     return true;
 }
