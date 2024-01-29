9734b48ddc88e4ec4027f591b458d849a8febf32
Alexandre De Almeida <91562207+Pikaleks@users.noreply.github.com>
2024-01-29 23:49:34 +0100

    FEAT: manage cookie

---
+++ front/src/libs/cookies.ts
@@ -0,0 +1,16 @@
+import Cookie from "universal-cookie";
+
+export function saveCookies(field: string, value: any) {
+    const cookies = new Cookie();
+    cookies.set(field, value, { path: "/", sameSite: true });
+}
+
+export function readFromCookies(field: string) {
+    const cookies = new Cookie();
+    return cookies.get(field);
+}
+
+export function removeCookies(field: string) {
+    const cookies = new Cookie();
+    return cookies.remove(field);
+}
