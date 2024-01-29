83974fd9d7b988c19cb5a776fdbc61f7d8434a51
Alexandre De Almeida <91562207+Pikaleks@users.noreply.github.com>
2024-01-29 16:29:19 +0100

    FIX: Layout for better style

--- front/src/Components/Dashboard/DashboardLayout.tsx
+++ front/src/Components/Dashboard/DashboardLayout.tsx
@@ -3,13 +3,13 @@ import { Outlet } from "react-router-dom";
 
 const DashboardLayout = (): JSX.Element => {
     return (
-        <div style={{ display: "flex", minHeight: "100%", overflow: "hidden"}}>
+        <div style={{ display: "flex", flexDirection: "column", minHeight: "100%"}}>
             <DashboardNavbar />
-            <div style={{ flex: 1, overflow: "auto", minHeight: "100%"}}>
+            <div style={{ flex: 1, overflow: "auto", minHeight: "100%", padding: "20px" }}>
                 <Outlet />
             </div>
         </div>
     );
 }
 
-export default DashboardLayout
+export default DashboardLayout;
